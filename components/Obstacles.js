9516a6bfdc8e9fefa413d2a7db6d1c3ce42b7e9d
Mateusz Kopij <sagasu@gmail.com>
2021-04-08 20:57:36 +0100

    obstacles are now pipes

--- components/Obstacles.js
+++ components/Obstacles.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet, ImageBackground} from 'react-native';
 
 const Obstacles = ({obstaclesLeft, obstacleWidth, obstacleHeight, gap, randomBottom}) => {
-
+    const styles = StyleSheet.create({
+        image: {
+            position: 'absolute',
+            width: obstacleWidth,
+            height: obstacleHeight,
+            left: obstaclesLeft,
+            bottom: randomBottom + obstacleHeight + gap,
+        },
+        imagedown: {
+            position: 'absolute',
+            width: obstacleWidth,
+            height: obstacleHeight,
+            left: obstaclesLeft,
+            bottom: randomBottom,
+        },
+    });
 
     return (
         <>
-            <View style={{
-                position: 'absolute',
-                backgroundColor: 'green',
-                width: obstacleWidth,
-                height: obstacleHeight,
-                left: obstaclesLeft,
-                bottom: randomBottom + obstacleHeight + gap,
-            }} />
+            <ImageBackground source={require('../assets/flappybird-pipe.png')} style={styles.image}>
+            </ImageBackground>
 
-            <View style={{
-                position: 'absolute',
-                backgroundColor: 'green',
-                width: obstacleWidth,
-                height: obstacleHeight,
-                left: obstaclesLeft,
-                bottom: randomBottom,
-            }} />
+            <ImageBackground source={require('../assets/flappybird-pipe.png')} style={styles.imagedown}>
+            </ImageBackground>
         </>)
 }
 
